52e854474f1462755c9c665afd2d19a9c15b1e52
maobasix <2270545457@qq.com>
2022-04-25 13:19:56 +0800

    第二次修改

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import openpyxl
+import datetime
 
-class I_O:
+
+class py_excel:
+    def __init__(self, path):
+        self.path = path
+
+    def add_excel(self):
+        cel = openpyxl.Workbook()
+        cel.save(self.path)
+
+    def del_excel(self):
+        pass
+
+    def alter_excel(self):
+        pass
+
+    def look_excel(self):
+        pass
 
 
- if __name__ == '__main__':
-     pass
+if __name__ == '__main__':
+    py_excel(input("输入路径")).add_excel()

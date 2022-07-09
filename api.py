ede1ed1de0080c47dc51f03e31b9b150eb932175
maobasix <2270545457@qq.com>
2022-07-09 22:44:12 +0800

    完成核心代码

--- api.py
+++ api.py
@@ -1,6 +1,14 @@
 import requests
 import datetime
 import json
+import tkinter as tk
+from PIL import Image, ImageTk
+import numpy as np
+from matplotlib import pyplot as plt
+from pylab import mpl  # 设置显示中文字体
+
+mpl.rcParams["font.sans-serif"] = ["SimHei"]
+mpl.rcParams["axes.unicode_minus"] = False  # 设置正常显示符号
 
 Data = {}
 
@@ -35,18 +43,90 @@ class Request_Data:  # 数据收集类
         return json_Data
 
 
-class Data_Processing:  # 数据处理类
-    def __init__(self):
+class Graphing:  # 绘制图形类,把数据做成图片的形式
+
+    def __init__(self, messages, Type=0):  # messages是个字典 Type 决定了该如何绘制图片
+        self.type = Type
+        self.messages = messages
+        if self.type == 0:
+            self.pyplt_0()
+        elif self.type == 1:
+            self.pyplt_1()
+        elif self.type == 2:
+            self.pyplt_2()
+        elif self.type == 3:
+            self.pyplt_3()
+        elif self.type == 4:
+            self.pyplt_4()
+
+    def pyplt_0(self):
+        message = self.messages
+        title = message['msg']['newslist'][0]['news'][0]['title']
+        print(message['msg']['newslist'][0]['news'][0]['title'])
+        x = np.array([title, "Runoob-2", "Runoob-3", "C-RUNOOB"])  # 标题
+        y = np.array([12, 22, 6, 18])  # 数据
+        plt.bar(x, y)
+        plt.savefig("debug.png")
+        return 0
+
+    def pyplt_1(self):
+        pass
+
+    def pyplt_2(self):
+        pass
+
+    def pyplt_3(self):
+        pass
+
+    def pyplt_4(self):
         pass
 
 
-class Graphing:  # 绘制图形类
-    pass
+class gui:  # gui界面
+    def __init__(self, messages):
+        self.messages = messages
+
+    def tkgui(self):
+        messages = self.messages
+        riskarea = messages['msg']['newslist'][0]['riskarea']['high'] + messages['msg']['newslist'][0]['riskarea'][
+            'mid']
+        print(riskarea)
+        root = tk.Tk()
+        sbar_right = tk.Scrollbar(root)  # 创建滚动条
+        sbar_right.pack(side="right", fill="y")  # 滚动条贴边
+        sbar_bottom = tk.Scrollbar(root, orient="horizontal")  # 创建滚动条
+        sbar_bottom.pack(side="bottom", fill="x")  # 滚动条贴底
+        msg = tk.Text(root, width=280, font=('微软雅黑', 10, 'bold'))
+        button_riskarea = tk.Button(root, text="第二个窗口", command=self.twogui)
+        button_off = tk.Button(root, text="关闭", command=root.quit)
+        tk.Label(root, text="风险地区", fg="red", font=('Times', 10, 'bold italic')).pack()
+        msg.pack()
+        tk.Message()
+        for i in riskarea:
+            msg.insert("end", i + "\n")
+        sbar_right.config(command=msg.yview)  # 滚动条绑定
+        sbar_bottom.config(command=msg.xview)  # 滚动条绑定
+        button_riskarea.pack()
+        button_off.pack(side="bottom")
+        root.title("疫情数据查询系统----by_曾大傻")
+        root.geometry('450x300')
+        root.mainloop()
+
+    def twogui(self):
+        root = tk.Toplevel()
+        root.title("二级窗口")
+        Graphing(a1)
+        photo = ImageTk.PhotoImage(Image.open('debug.png'))
+        print(photo)
+        Lab = tk.Label(root, text='第一个图片测试', compound='center', font=('微软雅黑', 30), image=photo)
+        Lab.pack()
+        root.mainloop()
 
 
-'''if __name__ == '__main__':
+if __name__ == '__main__':
     a = Request_Data('http://api.tianapi.com/ncov/index', '2020-02-12').Get_Info()
     a1 = json.loads(a)
     datetime.datetime.strptime('2020-02-12', '%Y-%m-%d')
-    print(a1)
-'''
+    print(type(a1))
+
+    gui(a1).tkgui()
--- main.py
+++ main.py
@@ -3,5 +3,6 @@ import json
 
 if __name__ == '__main__':
     resp = api.Request_Data('http://api.tianapi.com/ncov/index', '2020-05-28').Get_Info()
-    resp_json = json.loads(resp)
-    print(resp_json)
+    resp_dict = json.loads(resp)
+    print(resp_dict)
+    # print(resp_json['type'])
